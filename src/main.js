5c7c5ca00b567ced747676c7f2869d52a5282b10
Pavel Iugai <pavel.iugai@outlook.com>
2021-11-28 12:11:22 +0800

    Create componenets and render them

--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
-import { createMenu } from './view/menu.js';
+import { createSiteMenuTemplate } from './view/site-menu-view.js';
+import { createFiltersTemplate } from './view/filters-view.js';
+import { createSortTemplate } from './view/sort-view.js';
+import { createPointsListTemplate } from './view/points-list-view.js';
+import { createPointEditTemplate } from './view/point-edit-view.js';
+import { createPointsItemTemplate } from './view/points-item-view.js';
 import { renderTemplate, RenderPosition } from './render.js';
 
-const tripControls = document.querySelector('.trip-controls');
-const tripControlsNavigation = tripControls.querySelector('.trip-controls__navigation');
+const POINTS_COUNT = 3;
 
-renderTemplate( tripControlsNavigation, createMenu(), RenderPosition.BEFOREEND);
+const tripControlsElement = document.querySelector('.trip-controls');
+const tripControlsNavigationElement = tripControlsElement.querySelector('.trip-controls__navigation');
+
+renderTemplate( tripControlsNavigationElement, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
+
+const tripControlsFiltersElement = tripControlsElement.querySelector('.trip-controls__filters');
+
+renderTemplate( tripControlsFiltersElement, createFiltersTemplate(), RenderPosition.BEFOREEND);
+
+const tripEventsElement = document.querySelector('.trip-events');
+
+renderTemplate( tripEventsElement, createSortTemplate(), RenderPosition.BEFOREEND);
+renderTemplate( tripEventsElement, createPointsListTemplate(), RenderPosition.BEFOREEND);
+
+const tripEventsListElement = tripEventsElement.querySelector('.trip-events__list');
+
+renderTemplate( tripEventsListElement, createPointEditTemplate(), RenderPosition.BEFOREEND);
+
+for (let i = 0; i < POINTS_COUNT; i++) {
+  renderTemplate( tripEventsListElement, createPointsItemTemplate(), RenderPosition.BEFOREEND);
+}
