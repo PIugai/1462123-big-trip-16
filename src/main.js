500826c8465c8fd7eef8b6fd206b5fc913ea63fc
Pavel Iugai <pavel.iugai@outlook.com>
2021-11-28 10:40:05 +0800

    Rename menu template

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { createTripTabsTemplate } from './view/trip-tabs.js';
+import { createMenu } from './view/menu.js';
 import { renderTemplate, RenderPosition } from './render.js';
 
 const tripControls = document.querySelector('.trip-controls');
 const tripControlsNavigation = tripControls.querySelector('.trip-controls__navigation');
 
-renderTemplate( tripControlsNavigation, createTripTabs(), RenderPosition.BEFOREEND);
+renderTemplate( tripControlsNavigation, createMenu(), RenderPosition.BEFOREEND);
