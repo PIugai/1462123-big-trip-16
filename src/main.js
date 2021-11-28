f2db2a38f4aff8cb287a0c64a9ebde54932dd553
Pavel Iugai <pavel.iugai@outlook.com>
2021-11-28 10:35:51 +0800

    Create menu controls

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import { createTripTabsTemplate } from './view/trip-tabs.js';
 import { renderTemplate, RenderPosition } from './render.js';
 
-renderTemplate( , RenderPosition.BEFOREEND);
+const tripControls = document.querySelector('.trip-controls');
+const tripControlsNavigation = tripControls.querySelector('.trip-controls__navigation');
+
+renderTemplate( tripControlsNavigation, createTripTabs(), RenderPosition.BEFOREEND);
