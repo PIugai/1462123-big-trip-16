c0b9901da8aa1cd3832e0511fffc0798451ba22b
Pavel Iugai <pavel.iugai@outlook.com>
2022-02-07 10:17:29 +0800

    Delete unused filed/functions

--- src/utils/get-random-array-element.js
+++
@@ -1,6 +0,0 @@
-import { shuffleArray } from './shuffle-array.js';
-
-export const getRandomArrayElement = (elements) => {
-  const shuffledElements = shuffleArray(elements);
-  return shuffledElements[0];
-};
--- src/utils/shuffle-array.js
+++
@@ -1,5 +0,0 @@
-export const shuffleArray = (unmuffledArray) =>
-  unmuffledArray
-    .map((item) => ({ sort: Math.random(), value: item }))
-    .sort((a, b) => a.sort - b.sort)
-    .map((item) => item.value);
